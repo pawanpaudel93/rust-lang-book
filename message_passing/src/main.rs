f56d0c9653ffff67305a1987fecb8d5a7ca12528
Pawan Paudel <pawanpaudel93@gmail.com>
2023-01-14 23:20:14 +0545

    Completed 16.1 Threads & 16.2 Message Passing

---
+++ message_passing/src/main.rs
@@ -0,0 +1,54 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // Do not communicate by sharing memory; instead, share memory by communicating.
+    let (tx, rx) = mpsc::channel();
+    let handle = thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // println!("val is {}", val);
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+
+    handle.join().unwrap();
+
+    // Sending Multiple Values and Seeing the Receiver Waiting
+    let (tx, rx) = mpsc::channel();
+    let tx1 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
