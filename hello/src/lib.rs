e11f542af4d7a202afd6aa2694c9dcea8b2b4b1f
Pawan Paudel <pawanpaudel93@gmail.com>
2023-02-04 22:29:13 +0545

    Competed 20.3 Graceful Shutdown and Cleanup

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,3 +1,7 @@
+//! # hello
+//!
+//! `hello` is a library to create a thread pool.
+//!
 use std::{
     sync::{mpsc, Arc, Mutex},
     thread,
@@ -5,9 +9,33 @@ use std::{
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+/// Different kinds of message passed between sender and receiver.
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Sending terminate to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap()
+            }
+        }
+    }
 }
 
 impl ThreadPool {
@@ -30,28 +58,46 @@ impl ThreadPool {
         ThreadPool { workers, sender }
     }
 
+    /// Executes a new Job using a thread in the threadpool
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    /// Creates a new worker for the ThreadPool
+    ///
+    /// The id is the identifier for the new worker
+    /// The receiver is the single consumer shared among the threads in the pool.
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv().unwrap();
             println!("Worker {id} got a job; executing.");
-            job();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing", id);
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -21,11 +21,6 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    // let _http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
